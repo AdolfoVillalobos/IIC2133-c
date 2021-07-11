901a1b6d58f424e952c0f2d2efa1a6573dd80bfe
adolfrodeno <amvillalobos@uc.cl>
2021-07-11 10:02:03 -0400

    hello world and types

--- intro_to_c/src/1_tipos_basicos/main.c
+++ intro_to_c/src/1_tipos_basicos/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   // Type Int
   int x = 1287;
@@ -25,6 +25,30 @@ int main(int argc, char** argv)
   // Casting: float as int
   printf("Z = 2.721810 interpretado como int: %i\n\n", z);
   printf("Y = 'h' interpretado como int: %i\n\n", y);
+
+  // We can assign numerical values to char
+
+  y = 105;
+  printf("Y = 105 interpretado como int : %i\n\n", y);
+  printf("Y = 105 interpretado como char : %c\n\n", y);
+
+  // And operate as if it were a number
+  y = y - 1;
+  printf("Y = 104 interpretado como char %c\n\n", y);
+
+  // In arithmetic operations, integer division returns integer
+
+  double r = 5 / 3;
+  printf("5 /3 = %lf\n\n", r);
+
+  // To correctly interpret the integer division value, we need casting
+  r = (double)5 / (double)3;
+  printf("5 / 3 con casting = %lf \n\n", r);
+
+  // We can cast once
+
+  r = (double)5 / 3;
+  printf("5 / 3 con un solo casting = %lf\n\n", r);
 
   return 0;
 }
